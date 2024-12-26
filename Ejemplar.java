8a2b5764d4dfa25cb4d032b98d3f8464e00a11fa
Papaseit <161719291+Papaseit@users.noreply.github.com>
2024-12-26 20:41:44 +0100

    Add files via upload

---
+++ Ejemplar.java
@@ -0,0 +1,63 @@
+package paquete;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+public class Ejemplar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private paquete.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    @OneToMany(mappedBy = "ejemplar")
+    private Set<paquete.Prestamo> prestamos = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public paquete.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(paquete.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Set<paquete.Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<paquete.Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+}
